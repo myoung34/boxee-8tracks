f611d16680de833511dbb67f76ae223fc2f0b692
civascu <ivascucristian@gmail.com>
2011-11-22 01:11:04 +0200

    basic list layout, loading of mixes into list and play of the first song using boxee's default playing options

--- 8tracks/app.py
+++ 8tracks/app.py
@@ -1,8 +1,4 @@
 import mc
-from util import show_message
 mc.ShowDialogWait()
 mc.ActivateWindow(14000)
 mc.HideDialogWait()
-
-def sm():
-    show_message()
--- 8tracks/util.py
+++ 8tracks/util.py
@@ -1,4 +1,6 @@
 import mc
+import urllib
+import simplejson
 
 def show_message():
     mc.ShowDialogOk("MSG", 'Starting?')
@@ -15,3 +17,40 @@ def toggleTabs(tabIndex):
                 toggle.SetSelected(False);
         else:
             mc.GetActiveWindow().GetToggleButton(tabIndex).SetSelected(True)
+            
+def getFeaturedMixes():
+    featured_url = "http://8tracks.com/mixes.json?api_key=a07ee2f7cc1577f749ed10d2c796fc52515243cc&api_version=2&page=1"
+    fd = urllib.urlopen(featured_url)
+    mix_sets = simplejson.loads(fd.read())
+    listItems = mc.ListItems()
+    for mix in mix_sets["mixes"]:
+        item = mc.ListItem(mc.ListItem.MEDIA_AUDIO_OTHER)
+        item.SetArtist(str(mix["user"]["login"]))
+        year = mix['first_published_at'][1:4]
+        month = mix['first_published_at'][6:7]
+        day=mix['first_published_at'][9:10]
+        item.SetDate(int(year), int(month), int(day))
+        name = mix["name"].replace(u'\xbd', '')
+        item.SetTitle(str(name))
+        item.SetThumbnail(str(mix['cover_urls']['original']))
+        item.SetIcon(str(mix['cover_urls']['max200']))
+        
+        # get the real track url, so default actions like play would work
+        trackD = urllib.urlopen("http://8tracks.com/sets/460486803/play.json?mix_id=%s&api_key=a07ee2f7cc1577f749ed10d2c796fc52515243cc&api_version=2" % mix["id"])
+        trackInformation = simplejson.loads(trackD.read())
+        item.SetPath(str(trackInformation['set']['track']['url']))
+        item.SetProperty("id", str(mix['id']))
+        listItems.append(item)
+    mc.GetWindow(14000).GetList(201).SetItems(listItems)
+    mc.GetWindow(14000).GetList(201).Refresh()
+    mc.ShowDialogNotification("Loaded %s mixes" % listItems.size())
+    
+def playMix():
+    list = mc.GetActiveWindow().GetList(201)
+    items = list.GetItems()
+    selected = items[list.GetFocusedItem()]
+    selectedId = selected.GetProperty("id")
+    # get the actual url for this mix
+    
+    fd = urllib.urlopen("http://8tracks.com/sets/460486803/play.json?mix_id=438741&api_key=a07ee2f7cc1577f749ed10d2c796fc52515243cc&api_version=2")
+    mc.ShowDialogOk("Play", "Play %s with ID: %s" % (selected.GetTitle(), selectedId))
